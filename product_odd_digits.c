6ab130e6648c7514dd0b8e98190e5ea8c6998d44
harshita-8131 <pixim0099@gmail.com>
2026-08-29 23:59:27 +0530

    Add files via upload

---
+++ product_odd_digits.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, digit;
+    int product = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num > 0)
+    {
+        digit = num % 10;
+
+        if (digit % 2 != 0)
+        {
+            product = product * digit;
+        }
+
+        num = num / 10;
+    }
+
+    printf("Product of odd digits = %d\n", product);
+
+    return 0;
+}
