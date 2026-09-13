356288b701b6a013a45bd62967b22a355c5b2c31
harshita-8131 <pixim0099@gmail.com>
2026-09-13 12:02:28 +0530

    Add files via upload

---
+++ rotationofarray.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main()
+{
+int a[100], n, k;
+int i, j, temp;
+
+
+printf("Enter the number of elements: ");
+scanf("%d", &n);
+
+printf("Enter the elements:\n");
+for(i = 0; i < n; i++)
+{
+    scanf("%d", &a[i]);
+}
+
+printf("Enter the value of k: ");
+scanf("%d", &k);
+
+k = k % n;
+
+for(i = 1; i <= k; i++)
+{
+    temp = a[n - 1];
+
+    for(j = n - 1; j > 0; j--)
+    {
+        a[j] = a[j - 1];
+    }
+
+    a[0] = temp;
+}
+
+printf("Array after rotation:\n");
+
+for(i = 0; i < n; i++)
+{
+    printf("%d ", a[i]);
+}
+
+printf("\n");
+
+return 0;
+
+
+}
