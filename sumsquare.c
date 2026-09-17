5d3b239ac30266abe7f29aa6ce29844be8b7272c
harshita-8131 <pixim0099@gmail.com>
2026-09-17 23:44:01 +0530

    Add files via upload

---
+++ sumsquare.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+int a[10][10];
+int n, i, j;
+int sum = 0;
+
+
+printf("Enter the size of matrix: ");
+scanf("%d", &n);
+
+printf("Enter the matrix elements:\n");
+
+for (i = 0; i < n; i++)
+{
+    for (j = 0; j < n; j++)
+    {
+        scanf("%d", &a[i][j]);
+    }
+}
+
+for (i = 0; i < n; i++)
+{
+    sum = sum + a[i][i];
+}
+
+printf("Sum of main diagonal elements = %d\n", sum);
+
+return 0;
+
+
+}
+
