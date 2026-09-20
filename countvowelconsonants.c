886a7abd0bb47a04950251b2d6db242a9d8c7438
harshita-8131 <pixim0099@gmail.com>
2026-09-20 12:06:05 +0530

    Add files via upload

---
+++ countvowelconsonants.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i;
+    int vowels = 0;
+    int consonants = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' ||
+            str[i] == 'o' || str[i] == 'u' ||
+            str[i] == 'A' || str[i] == 'E' || str[i] == 'I' ||
+            str[i] == 'O' || str[i] == 'U')
+        {
+            vowels++;
+        }
+        else if ((str[i] >= 'a' && str[i] <= 'z') ||
+                 (str[i] >= 'A' && str[i] <= 'Z'))
+        {
+            consonants++;
+        }
+    }
+
+    printf("Number of vowels = %d\n", vowels);
+    printf("Number of consonants = %d\n", consonants);
+
+    return 0;
+}
